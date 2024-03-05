d9eeec236795e7581ead3d3b9b5e2e22a5a5726d
Leonardo Feiteira <leonardo.vianna.f@gmail.com>
2024-03-05 16:55:07 -0300

    🐛 Fix rebuild.sh to work on MacOS

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -8,16 +8,16 @@ rm -rf ".openapi-generator/FILES"
 
 # Rename operations that have duplicated names after we remove the "ExternalApi"
 # artifact below. Without this, both operations will be named the same and conflict.
-sed -i 's/ExternalApiFilesUploads_CreateUploadFileRequestAsync/CreateUploadFileRequestAsync/g' bin/swagger.json
-sed -i 's/ExternalApiRentalImageUploadRequests_CreateUploadFileRequestAsync/CreateRentalImageUploadFileRequestAsync/g' bin/swagger.json
-sed -i 's/ExternalApiRentalUnitImageUploadRequests_CreateUploadFileRequestAsync/CreateRentalUnitImageUploadFileRequestAsync/g' bin/swagger.json
-sed -i 's/ExternalApiLeaseLedgerChargesWrite_CreateCharge/CreateLeaseLedgerCharge/g' bin/swagger.json
-sed -i 's/ExternalApiOwnershipAccountLedgerCharges_CreateCharge/CreateOwnershipAccountLedgerCharge/g' bin/swagger.json
-sed -i 's/ExternalApiTaskHistoryFileUploads_CreateUploadFileRequestAsync/UploadTaskHistoryFileRequestAsync/g' bin/swagger.json
+sed -i='' 's/ExternalApiFilesUploads_CreateUploadFileRequestAsync/CreateUploadFileRequestAsync/g' bin/swagger.json
+sed -i='' 's/ExternalApiRentalImageUploadRequests_CreateUploadFileRequestAsync/CreateRentalImageUploadFileRequestAsync/g' bin/swagger.json
+sed -i='' 's/ExternalApiRentalUnitImageUploadRequests_CreateUploadFileRequestAsync/CreateRentalUnitImageUploadFileRequestAsync/g' bin/swagger.json
+sed -i='' 's/ExternalApiLeaseLedgerChargesWrite_CreateCharge/CreateLeaseLedgerCharge/g' bin/swagger.json
+sed -i='' 's/ExternalApiOwnershipAccountLedgerCharges_CreateCharge/CreateOwnershipAccountLedgerCharge/g' bin/swagger.json
+sed -i='' 's/ExternalApiTaskHistoryFileUploads_CreateUploadFileRequestAsync/UploadTaskHistoryFileRequestAsync/g' bin/swagger.json
 
 # Replace the weird "ExternalApiSomething" artifact in the operation names,
 # so that the generated code will be cleaner
-sed -i 's/ExternalApi.*_//g' bin/swagger.json
+sed -i='' 's/ExternalApi.*_//g' bin/swagger.json
 
 # Remove some enums where Buildium is returning invalid data
 jq 'del(.components.schemas.TenantMessage.properties.SMSOptInStatus.enum)' bin/swagger.json | sponge bin/swagger.json # See https://github.com/LeadSimple/LeadSimple/issues/11675
@@ -28,7 +28,7 @@ npx @openapitools/openapi-generator-cli generate -i bin/swagger.json -g ruby -o 
 # Remove the ApiError model: it is not used and conflicts with the ApiError exception class
 rm -rf lib/buildium-ruby/models/api_error.rb
 rm -rf spec/models/api_error_spec.rb
-sed -i "s/require 'buildium-ruby\/models\/api_error'//g" lib/buildium-ruby.rb
+sed -i='' "s/require 'buildium-ruby\/models\/api_error'//g" lib/buildium-ruby.rb
 
 # Run rubocop in these files if it exists, else fail and ask to install it
 command -v rubocop >/dev/null 2>&1 || { echo "I require rubocop to be installed, but it couldn't be find in your PATH.  Aborting." >&2; exit 1; }
